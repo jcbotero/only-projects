13c567f693c5c10533f4cc223023e436e8762bb3
Juan Botero <jucaob10@hotmail.com>
2023-09-04 20:20:36 -0500

    done

---
+++ js.js
@@ -0,0 +1,35 @@
+
+
+function siteOne() {
+    location.href = "https://space-tourism-multipage.vercel.app/";
+  }
+   
+  function siteTwo() {
+    location.href = "https://jcbotero.github.io/Tip-Calculator/";
+  }
+   
+  
+  function siteThree() {
+    location.href = "https://jcbotero.github.io/E-commerce-product-page/";
+  }
+  
+  function siteFour() {
+    location.href = "https://jcbotero.github.io/Chat-app-landing/";
+  }
+
+
+  function siteFive() {
+    location.href = "https://bolsa-de-valores-col.vercel.app/";
+  }
+
+  function git() {
+    location.href = "https://github.com/jcbotero";
+  }
+
+  function link() {
+    location.href = "https://www.linkedin.com/in/juan-camilo-botero-gutierrez-31009559/";
+  }
+
+  function insta() {
+    location.href = "https://www.instagram.com/jucaob10/";
+  }
